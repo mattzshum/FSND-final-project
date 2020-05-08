5efd7edf19849d8a0aa23fbc6cc252614f96d6fa
Matthew Shum <43683485+mattzshum@users.noreply.github.com>
2020-05-07 20:24:18 -0700

    fixed imports

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 
